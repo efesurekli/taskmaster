a057dc55153b2a2c775d2b4ac5510a13b4f41d6b
efe <efe@efes-pro.speedport_w_724v_09011603_06_006>
2020-07-22 00:19:38 +0200

    Make bot running in heroku prod

--- run.py
+++ run.py
@@ -86,13 +86,9 @@ def main():
         updater.bot.set_webhook(PROD_URL + token)
 
     updater.idle()
-
-
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is     non-blocking and will stop the bot gracefully.
-
-
 
 if __name__ == '__main__':
     main()
